9148a0ff1c4fe56beda1049c32a218ecdb272530
Kauã Lima <mavericksamp2018@gmail.com>
2024-09-16 10:31:14 -0300

    :sparkles: initial commit

---
+++ Sistema de Banco/main.py
@@ -0,0 +1,24 @@
+#> Autor: Kauã Lima
+#> Data: 16/09/2024
+
+#> Desafio Sistema Bancário
+#| Fomos contratados por um grande banco para desenvolver seu novo sistema.
+#| Esse banco deseja modernizar suas operações e para isso escolheu a linguargem Python.
+#| Para a primeira versão do sistema, devemos implementar apenas 3 operações: depósito, saque e extrato.
+
+#> Depósito
+#| Deve ser possível depositar valores positivos para a minha conta bancária.
+#| A v1 do projeto trabalha apenas com 1 usuário, dessa forma, não precisamos nos preocupar em
+#| identificar qual é o número da agência e conta bancária.
+#| Todos os depósitos devem ser armazenados em uma variável e exibidos na operação de extrato.
+
+#> Saque
+#| O sistema deve permitir realizar 3 saques diários com limite máximo de R$ 500,00 por saque.
+#| Caso o usuário não tenha saldo em conta, o sistema deve exibir uma mensagem informando que não será
+#| possível sacar o dinheiro por falta de saldo.
+#| Todos os saques deve mser armazenados em uma variável e exibidos na operação de extrato.
+
+#> Extrato
+#| Essa operação deve listar todos os depósitos e saques realizados na conta.
+#| No fim deve ser exibido o saldo atual da conta.
+#| Os valores devem ser exibidos utilizando o formato: R$ XXXX.XX, exemplo: R$ 1500.45
